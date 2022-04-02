2f6be9c957e2d3d4d806230672a49d746a516de3
christseng <tzuhsi.tseng@gmail.com>
2022-04-02 14:27:49 +0800

    refactor: refine stack structure

--- shared/stack.go
+++ shared/stack.go
@@ -4,20 +4,32 @@ type ListNodeStack struct {
 	stack []*ListNode
 }
 
-func (s *ListNodeStack) push(elements ...*ListNode) {
+func (s *ListNodeStack) push(element *ListNode) {
 	if s.stack == nil {
 		s.stack = make([]*ListNode, 0)
 	}
-	s.stack = append(s.stack, elements...)
+	s.stack = append(s.stack, element)
 }
 
 func (s *ListNodeStack) pop() *ListNode {
-	l := len(s.stack)
-	if l == 0 {
+	if s.isEmpty() {
 		return nil
 	}
 
+	l := len(s.stack)
 	result := s.stack[l-1]
 	s.stack = s.stack[:l-1]
 	return result
+}
+
+func (s *ListNodeStack) isEmpty() bool {
+	return len(s.stack) == 0
+}
+
+func (s *ListNodeStack) peek() *ListNode {
+	if s.isEmpty() {
+		return nil
+	}
+
+	return s.stack[len(s.stack)-1]
 }
