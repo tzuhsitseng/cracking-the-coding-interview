3c1725c58f8913e6eaed58e488212239d55284b0
christseng <tzuhsi.tseng@gmail.com>
2022-04-21 21:15:43 +0800

    feat: add heap of int

---
+++ shared/heap.go
@@ -0,0 +1,14 @@
+package shared
+
+type IntHeap []int
+
+func (h IntHeap) Len() int            { return len(h) }
+func (h IntHeap) Less(i, j int) bool  { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *IntHeap) Pop() interface{} {
+	size := len(*h)
+	res := (*h)[size-1]
+	*h = (*h)[:size-1]
+	return res
+}
